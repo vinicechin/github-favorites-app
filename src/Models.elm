a467efee29c4ec7a943ae7afcabad285aa133b9f
Vinicius Cechin <vinicechin@gmail.com>
2018-07-17 17:44:19 -0300

    Add urls to issue model

--- src/Models.elm
+++ src/Models.elm
@@ -7,8 +7,26 @@ module Models exposing (..)
 initialModel : Model
 initialModel =
     { issues =
-        [ Issue 0 "Teste Issue" "vinicechin" False "open" [ Label "label 1" "yellow", Label "label 2" "#e2c151" ]
-        , Issue 1 "Teste Issue 2 muito extensa para testar" "fulanocomnomecomprido" True "closed" [ Label "label 2" "#e2c151" ]
+        [ Issue
+            0
+            "Teste Issue"
+            "vinicechin"
+            "https://avatars1.githubusercontent.com/u/16272285?v=4"
+            "https://github.com/vinicechin"
+            False
+            "open"
+            [ Label "label 1" "yellow", Label "label 2" "#e2c151" ]
+            "https://github.com/vinicechin/angular-test/issues/1"
+        , Issue
+            1
+            "Teste Issue 2 muito extensa para testar"
+            "fulanocomnomecomprido"
+            "https://avatars1.githubusercontent.com/u/16272285?v=4"
+            "https://github.com/vinicechin"
+            True
+            "closed"
+            [ Label "label 2" "#e2c151" ]
+            "https://github.com/vinicechin/angular-test/issues/2"
         ]
     , sorter = "id"
     , owner = ""
@@ -28,9 +46,12 @@ type alias Issue =
     { id : Int
     , name : String
     , assignee : String
+    , assigneeImageUrl : String
+    , assigneeUrl : String
     , isFavorite : Bool
     , state : String
     , labels : List Label
+    , url : String
     }
 
 
