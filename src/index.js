57bc681fda782403d7668b75e8be107fee74ee60
Vinicius Cechin <vinicechin@gmail.com>
2018-07-17 10:49:24 -0300

    Add external libraries: ace-css, fontawesome, bootstrap and dependecies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './main.css';
+import 'ace-css/css/ace.css';
+import 'font-awesome/css/font-awesome.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import { Main } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
+
 
 Main.embed(document.getElementById('root'));
 
